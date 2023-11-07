123929c48b49de50047ea7ec25bd5282284e3f6d
celestinoalan <celestino.alan@gmail.com>
2023-11-07 23:27:55 +0100

    Add backend and update .gitignore

---
+++ backend/gpt.py
@@ -0,0 +1,38 @@
+import flask
+from typing import List, Dict
+from flask import Flask
+import json
+from openai import OpenAI
+
+app = Flask(__name__)
+
+with open("config.json", "r") as handle:
+    config = json.load(handle)
+
+conversation_history = [{"role": "system", "content": "You are a helpful assistant."}]
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_message = flask.request.json['message']
+    print(user_message)
+    conversation_history.append({"role": "user", "content": user_message})
+    print(conversation_history)
+    response = ask_chatgpt(messages=conversation_history)
+    print(response)
+    conversation_history.append({"role": "assistant", "content": response})
+    print(conversation_history)
+    return flask.jsonify({'response': response})
+
+
+def ask_chatgpt(messages: List[Dict[str, str]]) -> str:
+    client = OpenAI(api_key=config["openai-api-key"])
+    chat_completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    return chat_completion.choices[0].message.content
+
+
+if __name__ == "__main__":
+    app.run()
