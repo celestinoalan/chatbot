1083acb41f7ec2f4116f39b3431f25381e7c3cfd
andreycelestino <andrey@rotaexata.com.br>
2023-11-07 15:01:38 -0300

    front do chat

---
+++ frontend/pages/chat.vue
@@ -0,0 +1,14 @@
+<template>
+  <bubble-chat />
+</template>
+
+<script>
+import BubbleChatVue from "../components/BubbleChat.vue";
+
+export default {
+  components: [BubbleChatVue],
+};
+</script>
+
+<style>
+</style>
