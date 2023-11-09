189a3f48d734c7489f4832e101ae53ccbc8cc9d1
celestinoalan <celestino.alan@gmail.com>
2023-11-09 18:55:13 +0100

    Add cost calculation to app

--- backend/app.py
+++ backend/app.py
@@ -17,6 +17,23 @@ with open(here.parent / "config.json", "r") as handle:
 with open(here.parent / "system_message.txt") as handle:
     system_message = handle.read()
 
+costs = []
+
+def estimate_conversation_cost(conversation_history: List[Dict[str, str]]) -> float:
+    def _estimate_message_cost(message_text: str, role: str) -> float:
+        if role not in ("assistant", "user", "system"):
+            print("Role not supported")
+            return 0
+        cost = input_cost if role in ("system", "user") else output_cost
+        return len(message_text.split(" ")) * cost
+
+    input_cost = 0.03 * 4.91
+    output_cost = 0.06 * 4.91
+    total_cost = 0
+    for message in conversation_history:
+        total_cost += _estimate_message_cost(message_text=message["content"], role=message["role"])
+    return total_cost
+
 
 @app.route("/chat", methods=["POST"])
 def chat():
@@ -27,6 +44,8 @@ def chat():
     print(response)
     conversation_history.append({"role": "assistant", "content": response})
     print(conversation_history)
+    costs.append(estimate_conversation_cost(conversation_history=conversation_history))
+    print(sum(costs))
     return flask.jsonify({'message': conversation_history})
 
 
